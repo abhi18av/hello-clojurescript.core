a786b6ccacb3ddb02941134a1308953c3cd907f0
Abhinav Sharma (abhi) <abhi18av@gmail.com>
2019-08-21 22:36:41 +0530

    add a general roadmap for this series

--- project.clj
+++ project.clj
@@ -6,8 +6,8 @@
 
   :min-lein-version "2.7.1"
 
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.339"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.520"]]
 
   :source-paths ["src"]
 
