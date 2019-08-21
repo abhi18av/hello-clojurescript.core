75ddd6b4aec3375a1d50983dc89d6e7acb1ccc98
Abhinav Sharma (abhi) <abhi18av@gmail.com>
2019-08-21 19:43:28 +0530

    initial commit

---
+++ test/hello_clojurescript/core_test.cljs
@@ -0,0 +1,10 @@
+(ns hello-clojurescript.core-test
+    (:require
+     [cljs.test :refer-macros [deftest is testing]]
+     [hello-clojurescript.core :refer [multiply]]))
+
+(deftest multiply-test
+  (is (= (* 1 2) (multiply 1 2))))
+
+(deftest multiply-test-2
+  (is (= (* 75 10) (multiply 10 75))))
