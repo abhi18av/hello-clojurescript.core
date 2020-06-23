c4d0442d38647f8ada2295a686c269f0334d4190
Abhinav Sharma <abhi18av@gmail.com>
2020-06-23 11:42:33 +0530

    able to connect and run node in nwjs

--- project.clj
+++ project.clj
@@ -6,17 +6,15 @@
 
   :min-lein-version "2.7.1"
 
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.520"]]
 
   :source-paths ["src"]
 
-  :aliases {"fig"       ["trampoline" "run" "-m" "figwheel.main"]
-            "fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]
+  :aliases {"fig"       ["run" "-m" "figwheel.main"]
+            "fig:build" ["run" "-m" "figwheel.main" "-b" "dev" "-r"]
             "fig:min"   ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "dev"]
             "fig:test"  ["run" "-m" "figwheel.main" "-co" "test.cljs.edn" "-m" hello-clojurescript.test-runner]}
 
-  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.1.9"]
-                                  [com.bhauman/rebel-readline-cljs "0.1.4"]]
-                   }})
+  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.2.9"]]}})
 
